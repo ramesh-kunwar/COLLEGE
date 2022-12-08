4907f6637b6c4dc3cf6d93bfd23fc2c90c17d26f
Ramesh Kunwar <109467139+ramesh-kunwar@users.noreply.github.com>
2022-12-08 13:30:22 +0545

    Update and rename creationOfLinkedList to creationOfLinkedList.c
    creation of linked list

---
+++ D.S.A/LinkedList/creationOfLinkedList.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next; // Node type pointer
+};
+void linkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Data: %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+void main()
+{
+    // memory allocation for nodes in the linked list
+    struct Node *head = (struct Node *)(malloc(sizeof(struct Node)));
+    struct Node *second = (struct Node *)(malloc(sizeof(struct Node)));
+    struct Node *third = (struct Node *)(malloc(sizeof(struct Node)));
+    struct Node *fourth = (struct Node *)(malloc(sizeof(struct Node)));
+
+    // link first and second node
+    head->data = 7;
+    head->next = second;
+
+    // link second and third node
+    second->data = 10;
+    second->next = third;
+
+    // link third and fourth node
+    third->data = 100;
+    third->next = fourth;
+
+    // Terminate the linked list at fourth
+    fourth->data = 97;
+    fourth->next = NULL;
+
+    linkedListTraversal(head);
+    getch();
+}
+
