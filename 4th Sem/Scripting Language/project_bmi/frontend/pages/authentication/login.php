dac320675c7dfb78566e78b053e19e288c75691c
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-03-21 10:10:07 +0545

    login form created

--- 4th Sem/Scripting Language/project_bmi/frontend/pages/authentication/login.php
+++ 4th Sem/Scripting Language/project_bmi/frontend/pages/authentication/login.php
@@ -1,3 +1,40 @@
+
+<?php
+
+
+// taking data from form in php
+
+/**
+ * isset() - built in funciton in php whhich is used to check whether the field has value set or not
+ */
+$email_error = "";
+$password_error = "";
+
+  if (isset($_POST['login'])) {
+    # code...
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+  
+  
+    if ($email == "") {
+      $email_error = "Email field is blank <br>";
+    }else{
+      echo "Email is : " . $email;
+    }
+    if ($password == "") {
+      $password_error = "Password field is blank <br>" ;
+    }else{
+      echo "Password is : " .$password;
+    }
+  }
+
+ 
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -10,14 +47,22 @@
   </head>
   <body>
   <div class="container">
-      <h1 class="text-center py-5">Login Page</h1>
+      <h1 class="text-center py-5  ">Login Page</h1>
       <div class="row">
         <div class="col-md"></div>
         <div class="col-md">
-          <form>
+          <form method="POST" action="login.php">
             <div class="form-group">
               <label for="">Email</label>
               <input class="form-control my-2" name="email" type="email" />
+              <p class="text-danger font-italic" >
+                <?php 
+                  if($email_error){
+                    echo $email_error;
+                  }
+              
+                ?>
+              </p>
             </div>
             <div class="form-group">
               <label for="">Password</label>
@@ -26,10 +71,18 @@
                 name="password"
                 type="password"
               />
+             <p class="text-danger font-italic">
+             <?php 
+               
+               if($password_error){
+                 echo $password_error;
+               }
+             ?>
+             </p>
             </div>
 
 
-            <input type="submit" value="Log In" class="btn btn-success  col-12" />
+            <input type="submit" value="Log In" class="btn btn-success  col-12" name="login" />
 
        
           </form>
