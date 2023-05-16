5c989b69b9b650f7767a4de604510d33d70df052
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-05-16 11:51:15 +0545

    function in js

--- 4thSem/ScriptingLanguage/Lab 10 - Functions/script.js
+++ 4thSem/ScriptingLanguage/Lab 10 - Functions/script.js
@@ -21,3 +21,37 @@ if (checkPlace("Lalitpur")) {
 } else {
     console.log('Is not available');
 }
+
+
+// function without parameter and return type
+function grandTotal() {
+    let sum = 0;
+    const prices = [123, 324, 200, 500, 1234, 451];
+    for (let i = 0; i < prices.length; i++) {
+        sum += prices[i]
+    }
+    return sum;
+}
+
+console.log(" your grand total is : " + grandTotal());
+
+// function without parameter and without return type
+
+showMessage()
+function showMessage() {
+    let message = "Have a nice day"
+    console.log("Today's Message: " + message);
+}
+
+// function with parameter and without return type
+const users = ['dav', 'dav123'];
+function checkUser(username, password) {
+    if (username === users[0] && password === users[1]) {
+        console.log('login success');
+    } else {
+        console.log('login fail');
+    }
+}
+
+
+checkUser("dav", 'dav123')
