9dd8bc9b28d6c658ea1db3f57693413880682ef4
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-05-16 11:30:49 +0545

    folder renamed

--- 4thSem/ScriptingLanguage/Lab 10 - Functions/script.js
+++ 4thSem/ScriptingLanguage/Lab 10 - Functions/script.js
@@ -1,1 +1,23 @@
 // Function in JS
+
+const places = ['Kathmandu', "Lalitpur", "Bhaktapur"]
+
+
+function checkPlace(place) {
+    let status = false;
+    for (let i = 0; i < places.length; i++) {
+        if (places[i] == place) {
+            status = true;
+            break;
+        }
+    }
+    return status;
+}
+
+console.log(checkPlace("Kathmandu"));
+
+if (checkPlace("Lalitpur")) {
+    console.log("is available");
+} else {
+    console.log('Is not available');
+}
