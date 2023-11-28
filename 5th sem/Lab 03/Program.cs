a352a515136257f8f27bd0d981ed376ef9f56115
Ramesh Kunwar <109467139+ramesh-kunwar@users.noreply.github.com>
2023-11-28 08:26:03 +0545

    Lab 03

---
+++ 5th sem/Lab 03/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace lab3
+{
+
+
+    class Lab3
+    {
+        static void Add()
+        {
+            System.Console.WriteLine("Add Method without parameter");
+        }
+        static void Add(int x)
+        {
+            System.Console.WriteLine(x + 2);
+        }
+
+        static void Add(int x, int y)
+        {
+            System.Console.WriteLine(x + y);
+        }
+        static void Add(int x, int y, int z)
+        {
+            System.Console.WriteLine(x + y + z);
+        }
+        static void Main()
+        {
+            Add();
+            Add(5);
+            Add(5, 6);
+            Add(5, 6, 7);
+
+
+        }
+    }
+}
