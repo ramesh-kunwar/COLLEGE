d2f33cc2a0943056c0d25cafe34f60832306e3cc
ramesh-kunwar <rameshkunwar1110@gmail.com>
2024-06-08 07:38:51 +0545

    class files deleted

--- 6th Sem/Network programming/Lab01_Example2.java
+++ 6th Sem/Network programming/Lab01_Example2.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,29 +7,27 @@ import java.util.Map;
 public class Lab01_Example2 {
     public static void main(String[] args) {
         // String filePath = "/Users/username/logs/logfile.log";
-        String filePath = "./demo.log";
+        // String filePath = "./demo.log";
+        // String filePath = "/Library/Logs/LKDC-setup.log";
+        String filePath = "/Library/Logs/LKDC-setup.log";
         Map<String, Integer> pageVisitCounts = new HashMap<>();
 
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(filePath));
-
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(" ");
                 String page = parts[1];
-                Integer count = pageVisitCounts.get(page);
-
-                if (pageVisitCounts.containsKey(page)) {
-                    pageVisitCounts.put(page, count + 1);
-                } else {
-                    pageVisitCounts.put(page, 1);
-                }
+                Integer count = pageVisitCounts.getOrDefault(page, 0);
+                pageVisitCounts.put(page, count + 1);
             }
-        } catch (Exception e) {
-            // TODO: handle exception
+        } catch (IOException e) {
             e.printStackTrace();
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
 
+        // Output the page visit counts
+        for (Map.Entry<String, Integer> entry : pageVisitCounts.entrySet()) {
+            System.out.println("Page: " + entry.getKey() + " - Visits: " + entry.getValue());
+        }
     }
 }
